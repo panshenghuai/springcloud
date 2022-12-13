02b7942a12f6b4fef44098124a4952da7d6eb2f2
Huai <pansh9520@163.com>
2022-12-13 11:19:40 +0800

    开始学习配置中心,添加配置中心

---
+++ cloud-config-center3344/src/main/java/com/my/cloud/ConfigCenterApplication.java
@@ -0,0 +1,16 @@
+package com.my.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author psh
+ * @version 1.0
+ * @date 2022/12/13 11:17
+ */
+@SpringBootApplication
+public class ConfigCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApplication.class,args);
+    }
+}
